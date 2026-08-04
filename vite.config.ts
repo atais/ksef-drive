e1325b90acbe78d4e107586ae22bcabe308bd12a
Michał Siatkowski <atais.jr@gmail.com>
2026-08-05 00:33:57 +0200

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ksef-gdrive/',
+  base: '/ksef-drive/',
   plugins: [svelte()],
   server: {
     headers: {
